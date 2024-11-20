8a38c9f34804b29160647ddccdd4010537d46ae3
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-11-20 11:46:02 -0300

    Create SalaryService

---
+++ src/main/java/com/smartprocess/projCalcSalario/services/SalaryService.java
@@ -0,0 +1,24 @@
+package com.smartprocess.projCalcSalario.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smartprocess.projCalcSalario.entities.Employee;
+
+@Service
+public class SalaryService {
+
+	@Autowired
+	private TaxService taxService;
+	
+	@Autowired
+	private PensionService pensionService;
+	
+	
+	public double netSalary(Employee employee) {
+		
+		return employee.getGlossSalary() - taxService.tax(employee.getGlossSalary()
+				- pensionService.discount(employee.getGlossSalary()));
+	}
+	
+}
