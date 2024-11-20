d5cb24a7bcce9f3523e1a627a23bc34d1b50d791
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-11-20 11:24:59 -0300

    Project created

---
+++ src/main/java/com/smartprocess/projCalcSalario/ProjCalcSalarioApplication.java
@@ -0,0 +1,13 @@
+package com.smartprocess.projCalcSalario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjCalcSalarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjCalcSalarioApplication.class, args);
+	}
+
+}
